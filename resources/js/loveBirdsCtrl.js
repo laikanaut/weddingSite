59457ca880acc7621c2a9d046dc6c1d24c8b806d
laikanaut <ShalaRaan@toms-mac-mini.home>
2015-10-15 22:49:01 -0400

    Further progress on bridal party
    Currently works although CSS is a wanky clusterfuck at this point.
    Really do need to organize and clean

--- resources/js/loveBirdsCtrl.js
+++ resources/js/loveBirdsCtrl.js
@@ -7,7 +7,43 @@
         
        return{
            
-           templateUrl:'resources/templates/loveBirdsTemplate.html'
+           templateUrl:'resources/templates/loveBirdsTemplate.html',
+           link: function(scope, elem, attrs) {
+               
+                scope.bride = {
+                    userImg:"resources/img/people/katie_potter.png",
+                    userGender:0,  //0F - 1M
+                    userName:"Katie Potter",
+                    userAffil:0, //0K - 1J
+                    whoToCouple:"The Bride",
+                    school:"ud",
+                    title:"",
+                    role:"",
+                    desc:""
+
+                };
+               
+               scope.groom = {
+                    userImg:"resources/img/people/jimmy_meisterich.png",
+                    userGender:1,  //0F - 1M
+                    userName:"Jimmy Meisterich",
+                    userAffil:1, //0K - 1J
+                    whoToCouple:"The Groom",
+                    school:"ud",
+                    title:"",
+                    role:"",
+                    desc:""
+
+                };
+               
+               
+                scope.heartClick = function() {
+                    
+                   
+                   
+               }
+               
+           }
            
        }
         
